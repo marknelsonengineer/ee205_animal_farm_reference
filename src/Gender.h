030580ea8c6a9671bfc07568a000c7793187e8ed
Mark Nelson <marknels@hawaii.edu>
2022-04-06 10:01:52 -0400

    Refactor Gender to output to <<

--- src/Gender.h
+++ src/Gender.h
@@ -27,14 +27,26 @@ enum class Gender {
 };
 
 
-/// @param gender The gender `enum` to lookup
-inline std::string genderToString( Gender gender ) {
-   switch( gender ) {
-      case Gender::UNKNOWN_GENDER: return "Unknown gender";
-      case Gender::MALE:           return "Male";
-      case Gender::FEMALE:         return "Female";
+/// Output Gender as a formatted string
+///
+/// @param lhs_stream The output stream to write to (usually `cout`).  `lhs` stands for Left Hand Side and means the left side of the `<<` operator.
+/// @param rhs_Gender The Gender to output.  `rhs` stands for Right Hand Side and means the right side of the `<<` operator.
+/// @return `Unknown gender`, `Female` or `Male`.
+inline std::ostream& operator<<( std::ostream& lhs_stream, const Gender& rhs_Gender ) {
+   switch( rhs_Gender ) {
+      case Gender::UNKNOWN_GENDER:
+         lhs_stream << "Unknown gender";
+         break;
+      case Gender::MALE:
+         lhs_stream << "Male";
+         break;
+      case Gender::FEMALE:
+         lhs_stream << "Female";
+         break;
+      default:
+         /// @throw out_of_range If the enum is not mapped to a string.
+         throw std::out_of_range( PROGRAM_NAME ": Gender not mapped to a string" );
    }
 
-   /// @throw out_of_range If the enum is not mapped to a string.
-   throw std::out_of_range( PROGRAM_NAME ": Gender not mapped to a string" );
+   return lhs_stream;
 }
