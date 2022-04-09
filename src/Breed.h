ccdad669397eb086387c9ff14e8bd00da89f9876
Mark Nelson <marknels@hawaii.edu>
2022-04-09 16:21:07 -0400

    Fixup cat Breed enum to work with this codebase

--- src/Breed.h
+++ src/Breed.h
@@ -28,16 +28,23 @@ enum class Breed {
 } ;
 
 
-/// @param breed The breed `enum` to lookup
-inline std::string breedToString( Breed breed ) {
-   switch( breed ) {
-      case Breed::UNKNOWN_BREED: return "Unknown"    ;
-      case Breed::MAINE_COON   : return "Maine Coon" ;
-      case Breed::MANX         : return "Manx"       ;
-      case Breed::SHORTHAIR    : return "Shorthair"  ;
-      case Breed::PERSIAN      : return "Persian"    ;
-      case Breed::SPHYNX       : return "Sphynx"     ;
+/// Output Breed as a formatted string
+///
+/// @param lhs_stream The output stream to write to (usually `cout`).  `lhs` stands for Left Hand Side and means the left side of the `<<` operator.
+/// @param rhs_Breed  The Breed to output.  `rhs` stands for Right Hand Side and means the right side of the `<<` operator.
+/// @return `Unknown breed`, `Mane coon`, `Manx`, etc..
+inline std::ostream& operator<<( std::ostream& lhs_stream, const Breed& rhs_Breed ) {
+   switch( rhs_Breed ) {
+      case Breed::UNKNOWN_BREED: return lhs_stream << "Unknown"    ;
+      case Breed::MAINE_COON   : return lhs_stream << "Maine Coon" ;
+      case Breed::MANX         : return lhs_stream << "Manx"       ;
+      case Breed::SHORTHAIR    : return lhs_stream << "Shorthair"  ;
+      case Breed::PERSIAN      : return lhs_stream << "Persian"    ;
+      case Breed::SPHYNX       : return lhs_stream << "Sphynx"     ;
+      default:
+         /// @throw out_of_range If the enum is not mapped to a string.
+         throw std::out_of_range( PROGRAM_NAME ": Breed is not mapped to a string" );
    }
-   /// @throw out_of_range If the enum is not mapped to a string.
-   throw std::out_of_range( PROGRAM_NAME ": Breed not mapped to a string" );
+
+   return lhs_stream;
 }
