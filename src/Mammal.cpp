4c8f2901f283a6dee6cd36c42851ab4a9a0408d9
Mark Nelson <marknels@hawaii.edu>
2022-04-09 16:36:13 -0400

    Add Mammal to the codebase

---
+++ src/Mammal.cpp
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab10d_animal_farm_3 - EE 205 - Spr 2022
+///
+/// @file Mammal.cpp
+/// @version 1.0
+///
+/// @author Mark Nelson <marknels@hawaii.edu>
+/// @date   09_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include <string>
+
+#include "config.h"
+#include "Mammal.h"
+
+using namespace std;
+
+
+const std::string Mammal::MAMMAL_NAME = "Mammilian";  ///< @todo LOOK THIS UP
---
+++ src/Mammal.h
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab10d_animal_farm_3 - EE 205 - Spr 2022
+///
+/// Mammals have more fun
+///
+/// @file Mammal.h
+/// @version 1.0
+///
+/// @author Mark Nelson <marknels@hawaii.edu>
+/// @date   09_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "Animal.h"
+
+/// Mammals have more fun
+class Mammal : public Animal {
+public:   //////////////////////// Constants ///////////////////////////////////
+   static const std::string MAMMAL_NAME;  ///< The scientific name for Mammals
+
+public:   /////////////////////// Constructors ////////////////////////////////
+   /// Create a Mammal with the *minimum* required fields
+   Mammal( const Weight::t_weight newMaxWeight
+          ,const std::string&     newSpecies
+   ) : Animal( newMaxWeight, MAMMAL_NAME, newSpecies ) {};
+
+   /// Create a Mammal, populating *all* of the member variables
+   Mammal( const Gender           newGender
+          ,const Weight::t_weight newWeight
+          ,const Weight::t_weight newMaxWeight
+          ,const std::string&     newSpecies
+   ) : Animal( newGender, newWeight, newMaxWeight, MAMMAL_NAME, newSpecies ) {};
+};
