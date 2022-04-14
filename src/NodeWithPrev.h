11699a45f1984cb465c8051c2013a9ab18d78a79
Mark Nelson <marknels@hawaii.edu>
2022-04-14 13:17:59 -1000

    Migrate NodeWithPrev to a header-only class

--- src/NodeWithPrev.h
+++ src/NodeWithPrev.h
@@ -28,7 +28,47 @@ protected:  ////////////////////// Protected Members ///////////////////////////
                                   ///< `nullptr` if it's the first Node in the list.
 
 public:  /////////////////////////// Public Methods ////////////////////////////
-   void dump() const override;  ///< Output the contents of this object
-   bool validate() const noexcept override;  ///< Check the Node
+
+   /// Output the contents of this object
+   ///
+   /// #### Sample Output
+   /// @code
+   ///     ==============================================
+   ///     Cat     name                Kali
+   ///     Cat     gender              Female
+   ///     Cat     breed               Shorthair
+   ///     Cat     isFixed             false
+   ///     Cat     weight              1.3
+   /// @endcode
+   /// @todo update this output
+   ///
+   void dump() const override {
+      Node::dump();
+      FORMAT_LINE_FOR_DUMP( "NodeWithPrev", "prev" ) << prev << std::endl ;
+   }
+
+
+   /// Check the Node.  If something is not right, print out a message and
+   /// stop the validation.  It will not throw an exception.
+   ///
+   /// @return `true` if the Node is healthy
+   bool validate() const noexcept override {
+      Node::validate();
+
+      if( prev == nullptr ) {
+         return true;  /// `nullptr` is a valid value for the previous pointer.
+      }
+
+      /// @internal Perform a rudimentary recursive loop test and ensure
+      ///           the prev pointer does not refer back to itself.
+      ///           This also has the benefit of dereferencing the
+      ///           pointer and ensuring it points to a valid address.
+      if( prev == prev->prev ) {
+         std::cout << PROGRAM_NAME << ": Recursive loop detected:  prev points to itself!" ;
+         return false;
+      }
+
+      return true;
+   }
 
 };
