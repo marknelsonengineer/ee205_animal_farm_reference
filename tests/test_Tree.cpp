47ddf7bc1c5e77028fd1ecd3979072db52050256
Mark Nelson <marknels@hawaii.edu>
2022-04-29 16:46:08 -1000

    Fixup indentation. Stub out new test for erasing nodes from the tree

--- tests/test_Tree.cpp
+++ tests/test_Tree.cpp
@@ -23,32 +23,38 @@ using namespace std;
 
 BOOST_AUTO_TEST_SUITE( test_Tree )
 
-BOOST_AUTO_TEST_CASE( test_empty_Tree ) {
-   Tree testTree;  /// @todo Move into a test fixture
+   BOOST_AUTO_TEST_CASE( test_empty_Tree ) {
+   	Tree testTree;  /// @todo Move into a test fixture
 
-   BOOST_CHECK_EQUAL( testTree.empty(), true );
-   BOOST_CHECK_EQUAL( testTree.size(), 0 );
-   BOOST_CHECK_EQUAL( testTree.validate(), true );
-   BOOST_CHECK_NO_THROW( testTree.dump());
-}
-
-
-BOOST_AUTO_TEST_CASE( test_insert_Tree ) {
-   Tree testTree;
-
-   BOOST_CHECK_THROW( testTree.insert( nullptr ), invalid_argument );
-
-   for( int i = 0 ; i < 100 ; i++ ) {
-      Cat& aCat = Cat::generateCat();
-      BOOST_CHECK_NO_THROW( testTree.insert( &aCat ));
-      BOOST_CHECK_THROW( testTree.insert( &aCat ), logic_error );
-      BOOST_CHECK_EQUAL( testTree.empty(), false );
-      BOOST_CHECK_EQUAL( testTree.size(), i+1 );
-      BOOST_CHECK_EQUAL( testTree.isIn( &aCat ), true );
-      BOOST_CHECK_EQUAL( testTree.validate(), true );
+   	BOOST_CHECK_EQUAL( testTree.empty(), true );
+   	BOOST_CHECK_EQUAL( testTree.size(), 0 );
+   	BOOST_CHECK_EQUAL( testTree.validate(), true );
+   	BOOST_CHECK_NO_THROW( testTree.dump());
    }
 
-   // testTree.dump();
-}
+
+   BOOST_AUTO_TEST_CASE( test_insert_Tree ) {
+   	Tree testTree;
+
+   	BOOST_CHECK_THROW( testTree.insert( nullptr ), invalid_argument );
+
+   	for( int i = 0 ; i < 100 ; i++ ) {
+   		Cat& aCat = Cat::generateCat();
+   		BOOST_CHECK_NO_THROW( testTree.insert( &aCat ));
+   		BOOST_CHECK_THROW( testTree.insert( &aCat ), logic_error );
+   		BOOST_CHECK_EQUAL( testTree.empty(), false );
+   		BOOST_CHECK_EQUAL( testTree.size(), i+1 );
+   		BOOST_CHECK_EQUAL( testTree.isIn( &aCat ), true );
+   		BOOST_CHECK_EQUAL( testTree.validate(), true );
+   	}
+
+   	// testTree.dump();
+   }
+
+
+   BOOST_AUTO_TEST_CASE( test_erase_from_Tree ) {
+      Tree testTree;  /// @todo Move into a test fixture
+
+   }
 
 BOOST_AUTO_TEST_SUITE_END()
