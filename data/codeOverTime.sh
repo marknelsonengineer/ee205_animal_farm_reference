f46d156bd496c14927e438ca1fce31f6c71db6a7
Mark Nelson <marknels@hawaii.edu>
2022-05-18 12:38:03 -1000

    Issue #39:  Refining the prototype

--- data/codeOverTime.sh
+++ data/codeOverTime.sh
@@ -8,12 +8,17 @@ set terminal png size 1200,900
 set output 'codeOverTime.png'
 
 set grid
-set title 'Code Over Time'
-set yrange [50:160]
-set xlabel 'time (military)'
-set label 'finished walk' at 15, 140
-unset label
-set label 'finished walk' at 15, 105
-plot 'codeOverTime.csv' u 1:2 w lp t 'systolic', 'codeOverTime.csv' u 1:3 w lp t 'diastolic', 'codeOverTime.csv' u 1:4 w lp t 'heartrate'
+set title 'Progress of Animal Farm Source Code'
+#set yrange [0:1000]
+#set y2range [0:10]
+set y2tics 2
+set ytics nomirror
+set xrange [0:16]
+set xlabel 'Week'
+set ylabel 'Lines of Code'
+set y2label 'Lines/Test'
+plot 'codeOverTime.csv' u 1:2 w lp t 'Lines of Code' axis x1y1, 'codeOverTime.csv' u 1:3 w lp t 'Individual Tests' axis x1y1, 'codeOverTime.csv' u 1:5 w lp t 'Lines/Test' axis x1y2
 
 DONE
+
+# chmod +x codeOverTime.sh ; ./codeOverTime.sh
