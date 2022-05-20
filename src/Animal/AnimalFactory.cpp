c83e8063d043b1776b048aaf34f7457a3c39d327
Mark Nelson <marknels@hawaii.edu>
2022-05-19 23:34:37 -1000

    Issue #8:  Add Chicken & Finch to AnimalFactory

--- src/Animal/AnimalFactory.cpp
+++ src/Animal/AnimalFactory.cpp
@@ -18,19 +18,24 @@
 #include "AnimalFactory.h"
 #include "Mammal/Cat/Cat.h"
 #include "Mammal/Dog/Dog.h"
+#include "Bird/Finch.h"
+#include "Bird/Chicken.h"
 
 using namespace std;
 
 /// @internal This function uses `new` to create an Animal on the heap
 ///           Be sure to `delete` it when it's no longer needed
 Animal& AnimalFactory::generateAnimal() {
-   uniform_int_distribution<> animalRNG( 0, 1 );
+   uniform_int_distribution<> animalRNG( 0, 3 );
 
    Animal* newAnimal = nullptr;
 
    switch( animalRNG( ANIMAL_FARM_RNG ) ) {
       case 0: newAnimal = &(Cat::generateCat()) ; break;
       case 1: newAnimal = &(Dog::generateDog()) ; break;
+      case 2: newAnimal = new Finch( Bird::ConstructorType::RANDOM ) ; break;
+      case 3: newAnimal = new Chicken( Bird::ConstructorType::RANDOM ) ; break;
+      default: assert ( false );  // We should never get here
    }
 
    assert( newAnimal != nullptr );
