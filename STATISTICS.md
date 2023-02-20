ab489d40bb26b7d3ea19b3939ea37510e5b97d7d
Mark Nelson <marknels@hawaii.edu>
2023-02-20 11:04:42 -1000

    Update to latest stats.py.  Remove old stats.sh.

--- STATISTICS.md
+++ STATISTICS.md
@@ -1,16 +1,41 @@
 Project Statistics
 ==================
 
-| Type         | Files | Lines | Bytes | Working Lines | Working Bytes |
-|--------------|------:|------:|------:|--------------:|--------------:|
-|.h|20|1,440|54,160|560|21,260|
-|.cpp Sources|25|975|24,550|375|11,450|
-|.cpp Tests|12|684|18,708|348|10,836|
+| Type | Files | Lines | Bytes | Working Lines | Working Bytes |
+|------|------:|------:|------:|--------------:|--------------:|
+|.h Source|33|1,935|87,962|923|30,277|
+|.cpp Source|35|4,673|142,285|2,320|61,757|
+|Unit Tests|17|3,174|126,418|2,283|90,841|
+|Python|1|295|8,461|193|6,976|
+|HTML|1|35|721|n/a|n/a|
+|Markdown|7|207|13,101|n/a|n/a|
+|Images|12|n/a|2,401,096|n/a|n/a|
+|Other	Files|21|n/a|63,830|n/a|n/a|
 
-Number of unit tests:  71
+## Tags
+| Tag | Date	| Author	|
+|-----|------|--------|
+|v2.0|Sat Mar 26 04:25:21 2022 -1000|Mark Nelson|
+|0.1.0|Thu May 5 21:31:17 2022 -1000|Mark Nelson|
+|v1.0|Wed May 18 14:41:37 2022 -1000||
+|v3.0|Wed May 18 14:44:24 2022 -1000||
+|Animal-Farm-1|Wed May 18 14:59:58 2022 -1000||
+|Animal-Farm-2|Wed May 18 15:04:26 2022 -1000||
+|Animal-Farm-13|Thu May 19 23:35:57 2022 -1000|Mark Nelson|
+|Cat-Empire-3|Tue May 24 11:44:01 2022 -1000|Mark Nelson|
 
-Number of individual tests:  766
 
-Number of commits:  255
+## Software Engineering Metrics
 
-Automatically generated on Tue May 17 11:09:43 PM HST 2022
+Number of commits:  320
+
+Number of unit tests:  96
+
+Number of test points:  1133
+
+## GitHub	Statistics
+| Repository								  | Commits							| Issues						  | Social							|
+|-------------------------------------|---------------------------|-------------------------|---------------------------|
+| ![GitHub code size	in	bytes](https://img.shields.io/github/languages/code-size/marknelsonengineer/ee205_animal_farm_reference?style=social) <br/> ![GitHub repo size](https://img.shields.io/github/repo-size/marknelsonengineer/ee205_animal_farm_reference?style=social)	<br/>	![GitHub contributors](https://img.shields.io/github/contributors/marknelsonengineer/ee205_animal_farm_reference?style=social) | ![GitHub commit activity](https://img.shields.io/github/commit-activity/w/marknelsonengineer/ee205_animal_farm_reference?style=social) <br/> ![GitHub last	commit](https://img.shields.io/github/last-commit/marknelsonengineer/ee205_animal_farm_reference?style=social)	| ![GitHub	issues](https://img.shields.io/github/issues-raw/marknelsonengineer/ee205_animal_farm_reference?style=social) <br/> ![GitHub	closed issues](https://img.shields.io/github/issues-closed-raw/marknelsonengineer/ee205_animal_farm_reference?style=social) | ![GitHub forks](https://img.shields.io/github/forks/marknelsonengineer/ee205_animal_farm_reference?style=social) <br/> ![GitHub Repo	stars](https://img.shields.io/github/stars/marknelsonengineer/ee205_animal_farm_reference?style=social)	|
+
+Automatically generated on Mon 20 Feb 2023 11:02:11 AM 
--- bin/stats.py
+++ bin/stats.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+###############################################################################
+###         University of Hawaii, College of Engineering
+###         ee205_animal_farm - EE 205 - Spr 2023
+###
+### Generate project statistics
+###
+### @file   stats.py
+### @author Mark Nelson <marknels@hawaii.edu>
+###############################################################################
+
 import sys
 import datetime
 import subprocess
@@ -8,6 +18,12 @@ from git	import Repo
 import pathlib
 import os
 import locale
+import re
+
+
+GITHUB_USERNAME="marknelsonengineer"
+GITHUB_REPO="ee205_animal_farm_reference"
+STYLE="social"
 
 
 locale.setlocale(locale.LC_ALL, '')  # Use '' for auto
@@ -16,15 +32,19 @@ locale.setlocale(locale.LC_ALL, '')  # Use '' for auto
 repo = git.Repo(".")
 
 # Initialize the sets of file	types	we	will process
-h_files = set()
-c_files = set()
-cpp_files =	set()
+h_src_files = set()
+c_src_files = set()
+cpp_src_files = set()
+unit_test_files = set()
 py_files	= set()
 web_files =	set()
 asm_files =	set()
 markdown_files	= set()
 image_files	= set()
 other_files	= set()
+
+number_of_unit_tests = 0
+number_of_test_points = 0
 
 
 def print_title():
@@ -34,10 +54,6 @@ def print_title():
 
 
 def print_shields_io_tags():
-	GITHUB_USERNAME="marknelsonengineer"
-	GITHUB_REPO="DTMF_Decoder"
-	STYLE="social"
-
 	repo1=f"![GitHub code size	in	bytes](https://img.shields.io/github/languages/code-size/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
 	repo2=f"![GitHub repo size](https://img.shields.io/github/repo-size/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
 	repo3=f"![GitHub contributors](https://img.shields.io/github/contributors/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
@@ -87,8 +103,15 @@ def print_date():
 	print( "Automatically generated on " +	now.strftime("%c") )
 
 
-def process_gcc_files( rowName, fileList	):
-	print(len( fileList ))
+def remove_comments( line, sep="#" ):
+	lineStr = str(line, 'UTF-8')
+	i = lineStr.find( sep ) # Find the posision of the separator
+	if i >= 0 :
+		lineStr = lineStr[ 0:i ]
+	return lineStr.rstrip()
+
+
+def process_script_files( rowName, fileList ):
 	if( len( fileList ) == 0 ):
 		return
 
@@ -96,12 +119,52 @@ def process_gcc_files( rowName, fileList	):
 
 	totalBytes = 0
 	totalLines = 0
+	totalWorkingBytes = 0
+	totalWorkingLines = 0
+	for f in fileList:
+		totalBytes += os.path.getsize( f )
+		totalLines += sum(1 for line in open( f, "rb" ))
+		for line in open( f, "rb" ):
+			line = remove_comments( line, "#" )
+			if( len( line ) <= 0 ):
+				continue
+			totalWorkingLines += 1
+			totalWorkingBytes += len( line )
+			# print( line )
+
+	print( f"|{rowName}|{numFiles:n}|{totalLines:n}|{totalBytes:n}|{totalWorkingLines:n}|{totalWorkingBytes:n}|" )
+
+
+def process_gcc_files( rowName, fileList	):
+	if( len( fileList ) == 0 ):
+		return
+
+	numFiles = len( fileList )
+
+	totalBytes = 0
+	totalLines = 0
+	totalWorkingBytes = 0
+	totalWorkingLines = 0
 	for f in fileList:
 		totalBytes += os.path.getsize( f )
 		totalLines += sum(1 for line in open( f, "rb" ))
 		result = subprocess.run(['gcc', '-fpreprocessed', '-dD', '-w', '-E', f], stdout=subprocess.PIPE)
 
-	print( f"|{rowName}|{numFiles:n}|{totalLines:n}|{totalBytes:n}|n/a|n/a|" )
+		allOutput = ""
+#		lineStr = str(result, 'UTF-8')
+		for char in result.stdout.decode('ascii'):
+			allOutput += char
+
+		for line in allOutput.splitlines():
+			line = line.strip()
+
+			if( len( line ) <= 0 ):
+				continue
+			totalWorkingLines += 1
+			totalWorkingBytes += len( line )
+#			print( line )
+
+	print( f"|{rowName}|{numFiles:n}|{totalLines:n}|{totalBytes:n}|{totalWorkingLines:n}|{totalWorkingBytes:n}|" )
 
 
 def process_text_files( rowName, fileList	):
@@ -132,43 +195,83 @@ def process_data_files(	rowName,	fileList	):
 	print( f"|{rowName}|{numFiles:n}|n/a|{totalBytes:n}|n/a|n/a|" )
 
 
-def process_files():
+def process_files_in_git():
 	print( "| Type | Files | Lines | Bytes | Working Lines | Working Bytes |" )
 	print( "|------|------:|------:|------:|--------------:|--------------:|" )
 
-	for entry in repo.heads.master.commit.tree.traverse():
-		if	not os.path.isfile( entry.name ):
+	global number_of_unit_tests
+	global number_of_test_points
+
+	for (_path, _stage), entry in repo.index.entries.items():
+		# print( entry )
+
+		if	not os.path.isfile( _path ):
 			continue
 
-		file_extension	= pathlib.Path( entry.name	).suffix
-		print( entry.path + "***" + entry.name + "---" +	file_extension	)
-		if( file_extension == '.h'	):
-			h_files.add( entry.path	)
-		elif(	file_extension	==	'.c' ):
-			c_files.add( entry.path	)
-		elif(	file_extension	==	'.cpp' ):
-			cpp_files.add(	entry.path )
-		elif(	file_extension	==	'.py'	):
-			py_files.add( entry.path )
-		elif(	file_extension	in	['.html', '.css']	):
-			web_files.add(	entry.path )
-		elif(	file_extension	==	'.asm' ):
-			asm_files.add(	entry.path )
-		elif(	file_extension	==	'.md'	):
-			markdown_files.add( entry.path )
-		elif(	file_extension	in	['.svg',	'.png', '.ico', '.png',	'.jpg', '.jpeg', '.dot'] ):
-			image_files.add( entry.path )
-		else:
-			other_files.add( entry.path )
+		file_extension	= pathlib.Path( _path ).suffix
+		# print( "Path = " + _path + "    Name = " + "XXXX" + "   Extension = " +	file_extension	)
 
-	process_gcc_files( ".h", h_files )
-	process_gcc_files( ".c", c_files )
-	process_gcc_files( ".cpp", cpp_files )
+		# If it's a C++ unit test, then count the test cases and test points
+		if( _path[0:6] == "tests/" and file_extension in ['.h', '.c', '.cpp'] ):
+			unit_test_files.add( _path )
+			file = open( _path, "r")
+			for line in file:
+				if re.search("BOOST_.*_TEST_CASE", line):
+					number_of_unit_tests += 1
+					# print( line )
+				if re.search("BOOST_CHECK|BOOST_REQUIRE", line):
+					number_of_test_points += 1
+					# print( line )
+
+
+		elif( file_extension == '.h'	):
+			h_src_files.add( _path )
+		elif(	file_extension	==	'.c' ):
+			c_src_files.add( _path	)
+		elif(	file_extension	==	'.cpp' ):
+			cpp_src_files.add(	_path )
+		elif(	file_extension	==	'.py'	):
+			py_files.add( _path )
+		elif(	file_extension	in	['.html', '.css']	):
+			web_files.add(	_path )
+		elif(	file_extension	==	'.asm' ):
+			asm_files.add(	_path )
+		elif(	file_extension	==	'.md'	):
+			markdown_files.add( _path )
+		elif(	file_extension	in	['.svg',	'.png', '.ico', '.png',	'.jpg', '.jpeg', '.dot'] ):
+			image_files.add( _path )
+		else:
+			other_files.add( _path )
+
+	process_gcc_files( ".h Source", h_src_files )
+	process_gcc_files( ".c Source", c_src_files )
+	process_gcc_files( ".cpp Source", cpp_src_files )
+	process_gcc_files( "Unit Tests", unit_test_files )
+	process_script_files( "Python", py_files )
 	process_text_files( "HTML", web_files )
+	process_text_files( "Assembly", asm_files )
 	process_text_files( "Markdown", markdown_files )
 	process_data_files( "Images", image_files )
 	process_data_files( "Other	Files", other_files )
 
+
+def print_number_of_commits():
+	print( "" )
+	numberOfCommits = len(list(repo.iter_commits('HEAD')))
+	print( f"Number of commits:  {numberOfCommits}" )
+
+
+def print_test_stats():
+	global number_of_unit_tests
+	global number_of_test_points
+
+	if( number_of_unit_tests == 0 ):
+		return
+
+	print( "" )
+	print( f"Number of unit tests:  {number_of_unit_tests}" )
+	print( "" )
+	print( f"Number of test points:  {number_of_test_points}" )
 
 
 # The	Main Program
@@ -177,10 +280,16 @@ with open('STATISTICS.md',	'w') as redirected_output:
 	sys.stdout = redirected_output #	Redirect	stdout
 	print_title()
 
-	process_files()
+	process_files_in_git()
 
-#	print_tags()
+	print_tags()
 
-#	print_shields_io_tags()
+	print( "## Software Engineering Metrics" )
 
-#	print_date()
+	print_number_of_commits()
+
+	print_test_stats()
+
+	print_shields_io_tags()
+
+	print_date()
