7e5f787dc9ca3c56f69181d3c50e4f89fd4dcbf6
Mark Nelson <marknels@hawaii.edu>
2023-02-20 20:37:54 -1000

    Add a basic Hello World executable to the Farm

---
+++ src/hello.cpp
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+//         University of Hawaii, College of Engineering
+//         Animal Farm - EE 205 - Spr 2023
+//
+/// Generic Hello World application
+///
+/// @file    hello.cpp
+/// @author  Mark Nelson <marknels@hawaii.edu>
+///////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+/// A Hello World program
+///
+/// @see https://en.wikipedia.org/wiki/%22Hello%2C_World!%22_program
+///
+/// @return The exit value for the program
+int main() {
+   std::cout << "Hello, World!" << std::endl;
+   return 0;
+}
